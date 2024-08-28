a63b79ef6b33baa2357bffe1caa40fc47f3ca08f
Rajveer Singh <138320694+whitedevil7401@users.noreply.github.com>
2024-08-28 18:56:56 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,16 @@
+package adv_DS;
+public class reverse{
+    public static void main (String []args){
+        int number = 1234;
+        int reverseno = 0;
+
+        for(;number!=0; number = number/10)
+        {
+            int remainder = number %10;
+            reverseno = reverseno *10 + remainder ;
+
+        }
+        System.out.println("reverse number"+reverseno);
+
+    }
+};
